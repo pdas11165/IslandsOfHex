1c6d5c97f2a4c992568bad0f180a476c77dd2bab
pdas11165 <pdas11165@upei.ca>
2023-02-05 23:04:35 -0400

    28 test case passed. 3 left

--- src/main/java/islands/backend/GameModel.java
+++ src/main/java/islands/backend/GameModel.java
@@ -5,8 +5,12 @@ package islands.backend;
  *
  */
 public class GameModel {
-    Integer[][] playboard;
     public int size;
+    public int whiteScores;
+    public int blackScores;
+    public int[] parent;
+    public int[] rank;
+    Integer[][] playboard;
 
     public static final boolean WHITE = true;
     public static final boolean BLACK = false;
@@ -18,11 +22,18 @@ public class GameModel {
     public GameModel(int sz) {
         this.size = sz;
         playboard = new Integer[sz][sz];
+        parent = new int[sz*sz];
+        rank = new int[sz*sz];
+        for(int i = 0 ; i< sz*sz; i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
 
 
     }
 
-    /**
+
+        /**
      * Can a play be made at position row, col
      * @param row the row in question
      * @param col the col in question
@@ -49,23 +60,101 @@ public class GameModel {
      * @return true if the game is over and false otherwise
      * @throws IllegalArgumentException for invalid row and col
      */
+
+
     public boolean makePlay(int row, int col, boolean clr) {
-        return false;
+        int clrValue = clr == WHITE ? 1 : -1;
+        if (row < 0 || row >= playboard.length || col < 0 || col >= playboard.length) {
+            throw new IllegalArgumentException("Invalid Row or Column");
+        }
+
+        playboard[row][col] = clrValue;
+
+        int[][] directions = {{-1, -1}, {-1, 0}, {0, -1}, {0, 1}, {1, 0}, {1, 1}};
+        for (int[] direction : directions) {
+            int newRow = row + direction[0];
+            int newCol = col + direction[1];
+            if (newRow >= 0 && newRow < playboard.length && newCol >= 0 && newCol < playboard.length && playboard[newRow][newCol] != null) {
+                int root1 = find(newRow * playboard.length + newCol);
+                int root2 = find(row * playboard.length + col);
+                if (playboard[newRow][newCol] == 1 && clrValue == -1) {
+                    blackScores++;}
+                if (root1 != root2) {
+                    parent[root1] = root2;
+                    rank[root2] += rank[root1];
+                    if (clr == WHITE) {
+                        whiteScores--;
+                    } else {
+                        blackScores--;
+                    }
+                }
+            }
+        }
+
+        if (clr == WHITE) {
+            whiteScores++;
+        } else {
+            blackScores++;
+        }
+        int root = find(row * playboard.length + col);
+        boolean gameOver = false;
+
+        if (clrValue == 1) {
+            gameOver = rank[root] == playboard.length;
+            if (gameOver) {
+                whiteScores = 1;
+            }
+        } else {
+            gameOver = rank[root] == playboard.length;
+            if (gameOver) {
+                blackScores = 1;
+            }
+        }
+        return gameOver;
     }
+
+
 
     /**
      * Return the score for white
      * @return white score
      */
     public int whiteScore() {
-        return 0;
+        return whiteScores;
     }
+
 
     /**
      * return the score for black
      * @return black score
      */
     public int blackScore() {
-        return 0;
+        return blackScores;
     }
+
+private int find(int index){
+        while(index != parent[index]){
+            parent[index] = parent[parent[index]];
+            index = parent[index];
+        }
+        return index;
+    }
+
+    private void union(int p, int q){
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ){
+            return;
+        }
+        if (rank[rootP] < rank[rootQ]){
+            parent[rootP] = rootQ;
+            rank[rootQ] += rank[rootP];
+        }
+        else{
+            parent[rootQ] = rootP;
+            rank[rootP] += rank[rootQ];
+        }
+    }
+
+
 }
